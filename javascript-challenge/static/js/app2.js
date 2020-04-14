716c5cd262624727363db301422365f6bfdacd01
barbourleah15 <56279772+barbourleah15@users.noreply.github.com>
2020-04-13 20:32:53 -0400

    Add files via upload

--- javascript-challenge/static/js/app2.js
+++ javascript-challenge/static/js/app2.js
@@ -1,64 +1,37 @@
-// Get references to the tbody element, input fields and button
-let $tbody = document.querySelector("tbody");
-let $dateInput = document.querySelector("#datetime");
-let $cityInput = document.querySelector("#city");
-let $stateInput = document.querySelector("#state");
-let $countryInput = document.querySelector("#country");
-let $shapeInput = document.querySelector("#shape");
-let $durationMinutesInput = document.querySelector("#duration");
-let $commentsInput = document.querySelector("#comments");
-//let $searchBtn = document.querySelector("#search");
-//let $resetBtn = document.querySelector("#reset");
+// from tableData.js
+var tableData = data;
+let jQ = jQuery;
 
-// Add an event listener to the searchButton, call handleSearchButtonClick when clicked
-// Select the button
-let button = d3.select("#button");
+// YOUR CODE HERE!
 
-button.on("click", function() {
+let UFO = i => {
+	let row;
+	jQ('#ufoTable tbody').empty();
+	i.forEach(d => {
+		row = `
+			<tr>
+				<td>${d.datetime}</td>
+				<td>${d.city}</td>
+				<td>${d.state}</td>
+				<td>${d.country}</td>
+				<td>${d.shape}</td>
+				<td>${d.durationMinutes}</td>
+				<td>${d.comments}</td>
+			</tr>
+		`;
+		jQ('#ufoTable tbody').append(row);
+	})
+};
 
-// Create a copy of the data
-let tableData = data;
 
-// Build table with non-filtered data
-function renderTable() {
-  $tbody.innerHTML = "";
-  for (let i = 0; i < tableData.length; i++) {
-    // Get current address object and fields
-    let address = tableData[i];
-    console.log(address)
-    let fields = Object.keys(address);
-    // Create new row in tbody, set index to be i + startingIndex
-    let $row = $tbody.insertRow(i);
-    for (let j = 0; j < fields.length; j++) {
-      // For each field in address object, create new cell and set inner text to be current value at current address field
-      let field = fields[j];
-      let $cell = $row.insertCell(j);
-      $cell.innerText = address[field];
-    }
-  }
-}
+UFO(tableData);
 
-// Build search table for filtered data
-function handleSearchButtonClick() {
-  let filterDate = $dateInput.value;
-  
-  // Filter on date
-  if (filterDate != "") {
-    tableData = data.filter(function (address) {
-      let addressDate = address.datetime;
-      return addressDate === filterDate;
-    });
-  }
-  else { tableData };
+jQ('#filter-btn').click(() => {
+	let searchDate =  jQ('#datetime').val();
+	let searchResults = tableData.filter(d => d.datetime === searchDate);
+	UFO(searchResults);
+});
 
- // renderTable();
-}
-
-// Clear all the fields
-//function handleResetButtonClick(){
-//  renderTable();
-//}
-
-// Render the table for the first time on page load
-renderTable();
+jQ('#reset-btn').click(() => {
+	UFO(tableData);
 });
